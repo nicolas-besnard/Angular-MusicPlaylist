ef4026def88f3d5c72d4ca487a69b82633e301fc
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-12-14 00:05:55 +0000

    Create Directive for CurrentPlaylist #4

---
+++ app/scripts/CurrentPlaylist/CurrentPlaylist.directive.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+  .module('musicPlaylistApp')
+  .directive('currentPlaylist', CurrentPlaylist);
+
+function CurrentPlaylist() {
+  var directive = {
+    link: link,
+    templateUrl: 'scripts/CurrentPlaylist/CurrentPlaylist.directive.html',
+    restrict: 'AE'
+  };
+  return directive;
+
+  function link(scope, element, attrs) {
+    /* */
+  }
+}
